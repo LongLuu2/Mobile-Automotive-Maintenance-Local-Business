448dd37522d558dc021694ef8fae506d98addfca
longluu2 <longluu2@bu.edu>
2026-02-20 10:49:38 -0500

    info change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,6 @@ export default function Contact() {
   const contact = el('address', { class: 'contact' }, [
     el('div', {}, [el('strong', {}, ['Phone:']), ' (781) 526-4673']),
     el('div', {}, [el('strong', {}, ['Email:']), ' stfleurceleste@yahoo.fr']),
-    el('div', {}, [el('strong', {}, ['Address:']), ' High St, Everett, 02149'])
   ])
 
   return el('div', { class: 'card' }, [el('h2', { html: 'Contact' }), contact])
